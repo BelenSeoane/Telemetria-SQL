0992cd497173ff57e3480833e8c7b4d62f60bdf0
FacuVerge <facu.verge@gmail.com>
2022-06-17 18:09:03 -0300

    Creo archivo para entrega 3

---
+++ SQLQuery2.sql
@@ -0,0 +1,16 @@
+-------------------------------------------------------------------------
+--------------------- Grupo: SELECT_NAME_FROM_GROUP ---------------------
+-------------------------------------------------------------------------
+
+USE [GD1C2022]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+-- Creacion de Tablas BI sin FKs
+-- Agregado de FKs de tablas BI
+-- Creacion de Stored Procedures para migrar
+-- Ejecucion de la migracion
+-- Creacion de vistas
+-- Consultas para mostrar los items pedidos
